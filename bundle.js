14f8107cd35a5f75ca69d03fca7fde2f518c2155
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-11-17 10:05:56 +0100

    updated bundling

--- bundle.js
+++ bundle.js
@@ -1,26 +1,41 @@
 import caxa from "caxa";
 import pkg from "./package.json" assert {type: "json"};
+import {rmSync} from "fs";
+import {execSync} from "child_process";
+import {promisify} from "util";
+import ncp from "ncp";
+import {join} from "path";
 
+let base;
+rmSync("dist", {force: true, recursive: true});
+await new Promise((resolve, reject) => ncp(
+  ".",
+  "dist",
+  {
+    filter: f => {
+      if (!base) {
+        base = f;
+        return true
+      }
+      if (f.endsWith(".js.map")) return false;
+      return f.split(base)[1].match(/^\\(bin|js|sql|package.json|package-lock.json).*/);
+    }
+  },
+  e => e ? reject(e) : resolve()
+));
+execSync("npm ci --omit=dev", {
+  cwd: "dist/"
+});
+
+let output = `dist/WebifyBim-${pkg.version}-win-x64.exe`;
 caxa({
-  input: ".",
-  output: `dist/WebifyBim-${pkg.version}-win-x64.exe`,
+  input: "dist/.",
+  output,
   identifier: "webify-bim\\" + pkg.version,
   uncompressionMessage: "This may take a while to run the first time, please wait...",
-  exclude: [
-    "./obj",
-    "./stubs",
-    "./ts",
-    ".idea",
-    ".git",
-    ".gitignore",
-    "bundle.js",
-    "Converter.cs",
-    "tsconfig.json",
-    "WebifyBim.csproj"
-  ],
+  exclude: [output],
   command: [
     "{{caxa}}/node_modules/.bin/node",
-    "{{caxa}}/js/wrapper.js",
-    "{{caxa}}"
+    "{{caxa}}/js/app.js"
   ]
 }).catch(console.error);
