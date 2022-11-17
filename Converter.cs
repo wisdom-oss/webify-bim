6d71dd61b2faddf2e2b65e858976c2e868f5f684
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-11-17 10:05:24 +0100

    added conversion scripts for wexbim and xml

--- Converter.cs
+++ Converter.cs
@@ -1,6 +1,30 @@
-﻿namespace WebifyBim;
+﻿using System.IO;
+using System.Xml;
+using Xbim.Ifc;
+using Xbim.IO.Xml;
+using Xbim.ModelGeometry.Scene;
 
-public class Converter
-{
-    
-}
+var input = args[0];
+var wexbimOutput = args[1];
+var xmlOutput = args[2];
+
+using var model = IfcStore.Open(input);
+
+var context = new Xbim3DModelContext(model);
+context.CreateContext(null, false);
+
+System.Console.WriteLine("""{"logger": "wexbim", "msg": "Converting to WEXBIM now."}""");
+System.Console.WriteLine("""{"logger": "wexbim", "msg": "This may take some minutes."}""");
+using var wexBimFile = File.Create(wexbimOutput);
+using var wexBimBinaryWriter = new BinaryWriter(wexBimFile);
+model.SaveAsWexBim(wexBimBinaryWriter);
+System.Console.WriteLine("""{"logger": "wexbim", "msg": "Conversion done."}""");
+
+System.Console.WriteLine("""{"logger": "xml", "msg": "Converting to XML now."}""");
+System.Console.WriteLine("""{"logger": "xml", "msg": "This may take several minutes."}""");
+using var xmlFile = File.Create(xmlOutput);
+var xmlSettings = new XmlWriterSettings { Indent = true };
+using var xmlWriter = XmlWriter.Create(xmlFile, xmlSettings);
+var xbimXmlWriter = new XbimXmlWriter4(XbimXmlSettings.IFC4);
+xbimXmlWriter.Write(model, xmlWriter);
+System.Console.WriteLine("""{"logger": "xml", "msg": "Conversion done."}""");
